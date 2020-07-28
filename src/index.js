2ffeb8aaf8768c8cf443bf4796a4fb741f6633d5
Igor Santos <igor.sagitarius@gmail.com>
2020-07-28 17:39:21 -0300

    Criação das rotas

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter,Switch,Route} from 'react-router-dom'
+import CadastroVideo from './pages/cadastro/video'
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+     <Route path="/" component={App} exact />
+     <Route path="/cadastro/video" component={CadastroVideo} />
+     <Route component={() => (<div>Pagina 404</div>)} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
