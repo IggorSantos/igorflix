11ebd4ea3a2d56f89adb62e7f1405fcdfae381ee
Igor Santos <igor.sagitarius@gmail.com>
2020-08-26 16:05:40 -0300

    Mudanças no arquivo db

--- src/pages/cadastro/video/index.js
+++ src/pages/cadastro/video/index.js
@@ -1,11 +1,33 @@
 import React from 'react'
 import PageDefault from '../../../components/pageDefault'
-import {Link} from 'react-router-dom'
+import {Link,useHistory} from 'react-router-dom'
+import useForm from '../../../hooks'
+import FormField from '../../../components/formField'
+import Button from '../../../components/button'
 
 function CadastroVideo(){
+  const history = useHistory()
+  const {handleChange,values} = useForm({
+
+  })
   return(
     <PageDefault>
      <h1>Cadastro de videos</h1>
+      <form onSubmit={(event) => {
+       //event.preventDefault();
+       console.log("Video cadastrado com sucesso");
+       //history.push()
+     }}>
+     <FormField
+        label="Titulo do video"
+        name="titulo"
+        value={values.titulo}
+        onChange={handleChange}
+     />
+     <Button type="submit">
+         Cadastrar
+     </Button>
+     </form>
         <Link to="/cadastro/categoria">
         Cadastrar categoria
       </Link>
